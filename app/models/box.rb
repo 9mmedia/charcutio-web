1fc67d90df936941d93009751b9fadd781102e63
Danielle Sucher <dsucher@gmail.com>
2012-12-27 16:08:08 -0500

    Box#tweet

--- app/models/box.rb
+++ app/models/box.rb
@@ -5,4 +5,23 @@ class Box < ActiveRecord::Base
   has_many :data_points
   belongs_to :master_meat,
     class_name: Meat
+
+  def tweet(message, image_url)
+    Timeout::timeout(5) do
+      @response = TWITTER_CLIENT.post 'https://api.twitter.com/1.1/statuses/update.json',
+                                      {'status' => "#{message} #meat#{name_hashtag}"},
+                                      {'Accept' => 'application/xml'}
+    end rescue return nil
+    return tweet_successful?
+  end
+
+  private
+
+    def name_hashtag
+      " ##{name}" if name.present?
+    end
+
+    def tweet_successful?
+      @response ? @response.code.to_i == 200 : nil
+    end
 end
