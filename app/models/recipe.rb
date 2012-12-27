05e7ffcdd9cc7171a6a1dd78ebaddbd602d45d44
Danielle Sucher <dsucher@gmail.com>
2012-12-27 14:37:22 -0500

    Recipe#set_initial_water_percentage

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,17 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
   has_many :meats
   has_many :recipe_ingredients
+
+  validates :initial_water_percentage,
+    presence: true
+
+  before_validation :set_initial_water_percentage
+
+  def set_initial_water_percentage
+    total = 0
+    recipe_ingredients.each do |ingredient|
+      total += ingredient.recipe_weight_percentage * ingredient.water_percentage
+    end
+    self.initial_water_percentage = total
+  end
 end
