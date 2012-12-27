171bb007887195a54ced4281cb47258a37af15fc
Jeff Smith <jffreyjs@gmail.com>
2012-12-27 12:39:11 -0500

    sign in form on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 CharcutioWeb::Application.routes.draw do
-  root :to => 'pages#home'
   devise_for :users
-
+  root :to => 'pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
