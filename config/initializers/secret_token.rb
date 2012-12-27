ab8ea65c390f82ebf2ce110cec8ff15da1062a77
Jeff Smith <jffreyjs@gmail.com>
2012-12-27 11:01:45 -0500

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CharcutioWeb::Application.config.secret_token = '522e83d8dcd451419bda5dba6b1a3451f5a2ec028d068423aa3b844305efac730c6c99dcba4f963644cd0f1817df2e099923a0419022999685fd3d85760543d4'
