0a54f9c6c4d732cb399abbc61e74b9103a384ca1
Danielle Sucher <dsucher@gmail.com>
2012-12-28 13:11:36 -0500

    Fix typo in mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,22 +3,22 @@ class UserMailer < ActionMailer::Base
 
   def completed_meat_email(meat, team)
     @meat = meat
-    mail(to: team.users.map(:&email), subject: "Your meat is ready to eat!")
+    mail(to: team.users.map(&:email), subject: "Your meat is ready to eat!")
   end
 
   def curing_completed_email(meat, team)
     @meat = meat
-    mail(to: team.users.map(:&email), subject: "Your meat is done curing!")
+    mail(to: team.users.map(&:email), subject: "Your meat is done curing!")
   end
 
   def fermenting_completed_email(meat, team)
     @meat = meat
-    mail(to: team.users.map(:&email), subject: "Your meat is done fermenting!")
+    mail(to: team.users.map(&:email), subject: "Your meat is done fermenting!")
   end
 
   def meat_down_email(box, team, last_update_time)
     @box = box
     @last_update_time = last_update_time
-    mail(to: team.users.map(:&email), subject: "DANGER - MEAT DOWN!")
+    mail(to: team.users.map(&:email), subject: "DANGER - MEAT DOWN!")
   end
 end
