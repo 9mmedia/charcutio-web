2ae0389baf2ac1c7a9f2e268eda07f81f1a0a349
Danielle Sucher <dsucher@gmail.com>
2012-12-27 20:18:55 -0500

    Add RecipesController and basic views/actions

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,27 @@
+class RecipesController < ApplicationController
+
+  def create
+    if @recipe = Recipe.create_with_ingredients(params[:recipe])
+      redirect_to recipe_url @recipe
+    else
+      redirect_to root_url
+    end
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def show
+    @recipe = Recipe.find params[:id]
+  end
+
+  def update
+    @recipe = Recipe.find params[:id]
+    if @recipe && @recipe.update_with_ingredients(params[:recipe])
+      redirect_to recipe_url @recipe
+    else
+      redirect_to root_url
+    end
+  end
+end
