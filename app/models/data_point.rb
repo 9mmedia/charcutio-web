4388f40a3feae75de5f9528918d6bfc4cd47dd2c
Danielle Sucher <dsucher@gmail.com>
2012-12-28 16:06:50 -0500

    Add DataPoint scopes

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -1,3 +1,15 @@
 class DataPoint < ActiveRecord::Base
   belongs_to :box
+
+  def self.humidity
+    where data_type: 'humidity'
+  end
+
+  def self.temperature
+    where data_type: 'temperature'
+  end
+
+  def self.weight
+    where data_type: 'weight'
+  end
 end
