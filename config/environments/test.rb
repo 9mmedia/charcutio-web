a5122a0ce3279c72472ce65dd6c3006876f2fd66
Javier Julio <jjfutbol@gmail.com>
2013-06-26 02:51:49 -0400

    Add ember variant test to test env config otherwise it can't find scripts.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ CharcutioWeb::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # ember-rails states this is optional but if not defined scripts are not found
+  config.ember.variant = :test
 end
